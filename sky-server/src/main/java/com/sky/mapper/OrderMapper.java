3bda7f41cda53c51cfc8d93b466441d9e23b79f1
YangS <1499172392@qq.com>
2024-07-07 21:15:35 +0800

    管理端订单搜索、各个状态的订单数量统计、查询订单详情、接单、拒单、取消订单、派送订单、完成订单业务功能代码开发

--- sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -49,4 +49,7 @@ public interface OrderMapper {
      */
     @Select("select * from orders where id = #{id}")
     Orders getById(Long id);
+
+    @Select("select count(id) from orders where status = #{status}")
+    Integer countStatus(Integer status);
 }
