ff254c6587611c0a7fc2c63d8212da96adf244ab
YangS <1499172392@qq.com>
2024-07-08 15:44:13 +0800

    用户催单业务功能代码开发

--- sky-server/src/main/java/com/sky/service/OrderService.java
+++ sky-server/src/main/java/com/sky/service/OrderService.java
@@ -106,4 +106,11 @@ public interface OrderService {
      * @param id
      */
     void complete(Long id);
+
+    /**
+     * 用户催单
+     * @param id
+     * @return
+     */
+    void reminder(Long id);
 }
